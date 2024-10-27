2962840eab386f0a06ee829a0ce33031e4e8a154
Anli omar <omaranli56@gmail.com>
2024-10-27 21:39:52 +0400

    added create, update, and destroy methods to ReservationViewset

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
 from api.serializers import DishesSerializer, ReviewsSerializer, ReservationSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Dishes, Review, Reservation
 from .pagination import TenItemsPagination, TwelveItemsPagination
@@ -42,3 +45,26 @@ class ReservationViewSet(viewsets.ModelViewSet):
         if self.request.method.lower() == 'post' or self.request.method.lower() == 'get':
             return [permissions.AllowAny()]
         return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        try:
+            return super().update(request, *args, **kwargs)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
